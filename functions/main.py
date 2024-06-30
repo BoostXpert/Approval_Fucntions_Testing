c988959507453b17515238691bea44d98d42ad75
BoostXpert <114662158+BoostXpert@users.noreply.github.com>
2024-06-30 18:16:08 +0530

    Update main.py

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,10 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
+from flask import Flask
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+app = Flask(__name__)
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
